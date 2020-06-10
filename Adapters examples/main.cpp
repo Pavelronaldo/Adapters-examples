8c450aeab889c2c134ceaa5c71dfb1ef5942e815
Pavel <pavel.m.3107@gmail.com>
2020-06-10 10:20:03 +0300

    Adapters...

--- Adapters examples/main.cpp
+++ Adapters examples/main.cpp
@@ -6,10 +6,178 @@
 //  Copyright © 2020 Pavel. All rights reserved.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Classical Adapter
+struct Point {
+int32_t m_x;
+virtual void draw(){ cout<<"Point\n"; }
+};
+struct Point2D : Point {
+int32_t m_y;
+void draw(){ cout<<"Point2D\n"; }
+};
+void draw_point(Point &p) {
+p.draw();
 }
+struct Line {
+Point2D m_start;
+Point2D m_end;
+void draw(){ cout<<"Line\n"; }
+};
+struct LineAdapter : Point {
+Line& m_line;
+LineAdapter(Line &line) : m_line(line) {}
+void draw(){ m_line.draw(); }
+};
+int main() {
+Line l;
+LineAdapter lineAdapter(l);
+draw_point(lineAdapter);
+return EXIT_SUCCESS;
+}
+->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Bridge
+class Implementation {
+ public:
+  virtual ~Implementation() {}
+  virtual std::string OperationImplementation() const = 0;
+};
+
+class ConcreteImplementationA : public Implementation {
+ public:
+  std::string OperationImplementation() const override {
+    return "ConcreteImplementationA: Here's the result on the platform A.\n";
+  }
+};
+class ConcreteImplementationB : public Implementation {
+ public:
+  std::string OperationImplementation() const override {
+    return "ConcreteImplementationB: Here's the result on the platform B.\n";
+  }
+};
+
+
+class Abstraction {
+  /**
+   * @var Implementation
+   */
+ protected:
+  Implementation* implementation_;
+
+ public:
+  Abstraction(Implementation* implementation) : implementation_(implementation) {
+  }
+
+  virtual ~Abstraction() {
+  }
+
+  virtual std::string Operation() const {
+    return "Abstraction: Base operation with:\n" +
+           this->implementation_->OperationImplementation();
+  }
+};
+/**
+ * You can extend the Abstraction without changing the Implementation classes.
+ */
+class ExtendedAbstraction : public Abstraction {
+ public:
+  ExtendedAbstraction(Implementation* implementation) : Abstraction(implementation) {
+  }
+  std::string Operation() const override {
+    return "ExtendedAbstraction: Extended operation with:\n" +
+           this->implementation_->OperationImplementation();
+  }
+};
+
+
+void ClientCode(const Abstraction& abstraction) {
+  // ...
+  std::cout << abstraction.Operation();
+  // ...
+}
+
+int main() {
+  Implementation* implementation = new ConcreteImplementationA;
+  Abstraction* abstraction = new Abstraction(implementation);
+  ClientCode(*abstraction);
+  std::cout << std::endl;
+  delete implementation;
+  delete abstraction;
+
+  implementation = new ConcreteImplementationB;
+  abstraction = new ExtendedAbstraction(implementation);
+  ClientCode(*abstraction);
+
+  delete implementation;
+  delete abstraction;
+
+  return 0;
+}
+
+->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Decorator
+
+class Component {
+ public:
+  virtual ~Component() {}
+  virtual std::string Operation() const = 0;
+};
+
+class ConcreteComponent : public Component {
+ public:
+  std::string Operation() const override {
+    return "ConcreteComponent";
+  }
+};
+
+class Decorator : public Component {
+ protected:
+  Component* component_;
+
+ public:
+  Decorator(Component* component) : component_(component) {
+  }
+
+  std::string Operation() const override {
+    return this->component_->Operation();
+  }
+};
+
+class ConcreteDecoratorA : public Decorator {
+public:
+  ConcreteDecoratorA(Component* component) : Decorator(component) {
+  }
+  std::string Operation() const override {
+    return "ConcreteDecoratorA(" + Decorator::Operation() + ")";
+  }
+};
+
+class ConcreteDecoratorB : public Decorator {
+ public:
+  ConcreteDecoratorB(Component* component) : Decorator(component) {
+  }
+
+  std::string Operation() const override {
+    return "ConcreteDecoratorB(" + Decorator::Operation() + ")";
+  }
+};
+
+void ClientCode(Component* component) {
+  // ...
+  std::cout << "RESULT: " << component->Operation();
+  // ...
+}
+
+int main() {
+  Component* simple = new ConcreteComponent;
+  std::cout << "Client: I've got a simple component:\n";
+  ClientCode(simple);
+  std::cout << "\n\n";
+  Component* decorator1 = new ConcreteDecoratorA(simple);
+  Component* decorator2 = new ConcreteDecoratorB(decorator1);
+  std::cout << "Client: Now I've got a decorated component:\n";
+  ClientCode(decorator2);
+  std::cout << "\n";
+
+  delete simple;
+  delete decorator1;
+  delete decorator2;
+
+  return 0;
